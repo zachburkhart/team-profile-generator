737a1134d656f42e524d78769364560449dec079
Zach Burkhart <zachburkhart216@gmail.com>
2022-06-19 14:53:40 -0400

    added employee questions

---
+++ index.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateSite = require('./util/generateSite');
+
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+
+const questions = employeeQuestions => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Please enter your name',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('You need to enter a name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Please enter your ID',
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+                } else {
+                    console.log('You need to enter an ID!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your email',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('You need to enter an email!');
+                    return false;
+                }
+            }
+        },
+    ])
+}
